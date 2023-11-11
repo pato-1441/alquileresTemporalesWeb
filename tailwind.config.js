1692a63bd571258330bb2648bfe059032fc26ae4
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-11-11 13:21:35 -0300

    iniciando pagina web de ayuda

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        alquileres: ["'Lato', sans-serif", "'Work Sans', sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
